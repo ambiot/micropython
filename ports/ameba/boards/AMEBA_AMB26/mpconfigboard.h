3dabd1845cdb938a5dbf7c8d341f09cac651bb28
Dakamaster <644165318.tj@gmail.com>
2023-04-25 09:33:08 +0800

    Add new board AMB26 (#9)
    Co-authored-by: Zhu Qi <zhuqi@realtek-sg.com>

---
+++ ports/ameba/boards/AMEBA_AMB26/mpconfigboard.h
@@ -0,0 +1,5 @@
+//https://www.amebaiot.com/en/amebad/#rtk_amb21
+//https://www.amebaiot.com/en/amebad-arduino-getting-started/
+
+#define MICROPY_HW_BOARD_NAME    "AMB26"
+#define MICROPY_HW_MCU_NAME      "RTL8720DF"
